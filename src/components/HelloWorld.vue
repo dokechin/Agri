b2942f2ff9e9153073724370a876984e375cd337
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-08-22 22:44:35 +0900

    first commit

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,33 +1,6 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-unit-jest" target="_blank" rel="noopener">unit-jest</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1>{{ bleStatus }}</h1>
   </div>
 </template>
 
@@ -36,6 +9,59 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data: function(){
+    return {
+      USER_SERVICE_UUID: '作成したLINE TthingsのサービスUUID',
+      LED_CHARACTERISTIC_UUID: '独自定義したUUID', // LINE THings Starterのもので大丈夫
+      bleConnect: false,
+      bleStatus: `デバイスに接続されるのをお待ち下さい。`,
+      user: {
+        image: '',
+        userId: ''
+      }
+    }
+  },
+  methods: {
+    //BLEが接続できる状態になるまでリトライ
+    liffCheckAvailablityAndDo: async function (callbackIfAvailable) {
+      try {
+        const isAvailable = await this.$liff.bluetooth.getAvailability();
+        if (isAvailable) {
+          callbackIfAvailable();
+        } else {
+          // リトライ
+          this.bleStatus = `Bluetoothをオンにしてください。`;
+            setTimeout(() => this.liffCheckAvailablityAndDo(callbackIfAvailable), 10000);
+        }
+      } catch (error) {
+        alert('Bluetoothをオンにしてください。');
+      }
+    },
+    //サービスとキャラクタリスティックにアクセス
+    liffRequestDevice: async function () {
+      const device = await this.$liff.bluetooth.requestDevice();
+      await device.gatt.connect();
+      const service = await device.gatt.getPrimaryService(this.USER_SERVICE_UUID);
+      window.ledCharacteristic = await service.getCharacteristic(this.LED_CHARACTERISTIC_UUID);
+      // alert('connect');
+      this.bleConnect = true;
+      this.bleStatus = `デバイスに接続しました。`;
+    },
+    initializeLiff: async function(){
+      await this.$liff.initPlugins(['bluetooth']);
+      this.liffCheckAvailablityAndDo(() => this.liffRequestDevice());
+
+      //プロフゲット
+      const profile = await this.$liff.getProfile();
+      this.user.image = profile.pictureUrl;
+      this.user.userId = profile.userId;
+    },
+  },
+  mounted: function(){
+    this.$liff.init(
+      () => this.initializeLiff()
+    );
   }
 }
 </script>
--- src/main.js
+++ src/main.js
@@ -3,6 +3,8 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 
+Vue.prototype.$liff = window.liff
+
 Vue.config.productionTip = false
 
 new Vue({
