ed21c0e21572e8b9908c0117ff3c60ec04e66ab8
dokechin <ttatsumi@ra2.so-net.ne.jp>
2019-12-15 19:00:49 +0900

    commit

--- esp32/agri/agri.ino
+++ esp32/agri/agri.ino
@@ -3,6 +3,10 @@
 #include <BLEUtils.h>
 #include <BLE2902.h>
 
+#include <SPI.h>
+#include <EPD1in54.h>
+#include <EPDPaint.h>
+
 #include <stdint.h>
 
 // Device Name: Maximum 30 bytes
@@ -18,6 +22,8 @@
 #define PSDI_SERVICE_UUID "E625601E-9E55-4597-A598-76018A0D293D"
 #define PSDI_CHARACTERISTIC_UUID "26E2B12B-85F0-4F3F-9FDD-91D114270E6E"
 
+#define COLORED     0
+#define UNCOLORED   1
 
 BLEServer* thingsServer;
 BLESecurity *thingsSecurity;
@@ -29,13 +35,18 @@ BLECharacteristic* notifyCharacteristic;
 
 bool deviceConnected = false;
 bool oldDeviceConnected = false;
-const int PIN_NUM = 25;
+const int PIN_NUM = 34;
 const int ANALOG_MAX = 4096;
-const int TRY_COUNT = 10;
+const int TRY_COUNT = 5;
 
 uint8_t data[24*7];
+char humidityStr[24];
 
-unsigned long prev;
+unsigned long prev = 0;
+
+unsigned char image[40000];
+EPDPaint paint(image, 0, 0);    // width should be the multiple of 8
+EPD1in54 epd(33, 25, 26, 27); // reset, dc, cs, busy
 
 class serverCallbacks: public BLEServerCallbacks {
   void onConnect(BLEServer* pServer) {
@@ -55,11 +66,11 @@ class readCallback: public BLECharacteristicCallbacks {
 };
 
 void setup() {
-  Serial.begin(115200);
+  Serial.begin(9600);
 
   Serial.print("setup()");
 
-  pinMode(4, OUTPUT);
+  pinMode(12, OUTPUT);
 
   BLEDevice::init("");
   BLEDevice::setEncryptionLevel(ESP_BLE_SEC_ENCRYPT_NO_MITM);
@@ -73,27 +84,61 @@ void setup() {
   setupServices();
   startAdvertising();
   Serial.println("Ready to Connect");
+
+  if (epd.init(lutFullUpdate) != 0) {
+    Serial.print("e-Paper init failed");
+    return;
+  }
+  
+  epd.clearFrameMemory(0xFF);   // bit set = white, bit reset = black
+  epd.displayFrame();
+  epd.clearFrameMemory(0xFF);   // bit set = white, bit reset = black
+  epd.displayFrame();
 }
 
 void loop() {
   unsigned long now = millis();
   // every 1 hours
   if (now - prev > 1000 * 60 * 60){
-
+    
     prev = now;
     memcpy(data, &data[1], sizeof(data) - sizeof(int8_t));
 
-    digitalWrite(4, HIGH);
+    digitalWrite(12, HIGH);
 
     int sum = 0;
     for (int i=0;i<TRY_COUNT;i++){
       delay(1000);
-      sum = sum + analogRead(PIN_NUM);
+      int value = analogRead(PIN_NUM);
+      Serial.println(value);
+      
+      sum = sum + value;
     }
     
-    digitalWrite(4, LOW);
+    digitalWrite(12, LOW);
 
-    data[24*7 -1] = ((sum / TRY_COUNT) * 255)/ ANALOG_MAX;
+    uint8_t humidity = ((sum / TRY_COUNT) * 255)/ ANALOG_MAX;
+    humidity = ~humidity;
+//  uint8_t humidity = random(256);
+    data[24*7 -1] = humidity;
+
+    sprintf(humidityStr, "%03d", humidity);
+    Serial.println(humidityStr);
+
+    paint.setRotate(ROTATE_0);
+    paint.setWidth(200);
+    paint.setHeight(200);
+  
+    paint.clear(UNCOLORED);
+    paint.drawStringAt(0, 20, humidityStr, &Font24, COLORED);
+
+    for(int i=0;i<168;i++){
+      paint.drawLine(i + 15 , 199, i+ 16, int(199 - (data[i] /255.0) * 168), COLORED);    
+    }
+      
+    epd.setFrameMemory(paint.getImage(), 0, 0, paint.getWidth(), paint.getHeight());
+    epd.displayFrame();
+
   }
   // Disconnection
   if (!deviceConnected && oldDeviceConnected) {
